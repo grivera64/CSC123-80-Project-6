b16eb444a7bca26c7225f2f387b431f155d047b9
Giovanni Rivera <grivera64@users.noreply.github.com>
2021-03-27 22:35:45 -0700

    Add Rider.java
    Represent Riders in the Park

---
+++ park/Rider.java
@@ -0,0 +1,68 @@
+/*
+
+    Name: Rider.java
+    Purpose: Represents the basic outline of a rider
+    Author: grivera64
+
+*/
+
+public abstract Rider
+{
+
+    private int startOnlineTime;
+    private int endOnlineTime;
+    private int timeOnLine;
+    
+    public Rider()
+    {
+    
+        //default constructor
+    
+    }
+    
+    public Rider(int startOnlineTime)
+    {
+    
+        this.setStartOnlineTime(startOnlineTime);
+    
+    }
+    
+    public void setStartOnlineTime(int startOnlineTime)
+    {
+    
+        this.startOnlineTime = startOnlineTime;
+    
+    }
+    
+    public void setEndOnlineTime(int endOnlineTime)
+    {
+    
+        this.endOnlineTime = endOnlineTime;
+    
+    }
+    
+    public int getStartOnlineTime()
+    {
+    
+        return this.startOnlineTime;
+    
+    }
+    
+    public int getEndOnlineTime()
+    {
+    
+        return this.endOnlineTime;
+    
+    }
+    
+    public int getTimeOnLine()
+    {
+    
+        return this.timeOnLine;
+    
+    }
+    
+    public abstract void setType();
+    public abstract int getType();
+
+}
